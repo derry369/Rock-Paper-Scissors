6bd0d9756a224289048348383d0867f2dd92195d
Derrick Danso <derrickdansodd3@gmail.com>
2023-03-24 10:27:02 +0000

    Task completed

--- script.js
+++ script.js
@@ -5,40 +5,80 @@ function getComputerChoice(array) {
    const choice = array[randNum];
    return choice;
 }
+ 
 
-// console.log( getComputerChoice(arr) ); 
-
-
-function playRound(playerSelection, computerSelection) {
-    const finSelection = playerSelection.toUpperCase();
+function playRound(finSelection, computerSelection) {
     let result;
     const comBine = finSelection + ' ' + computerSelection;
-    // console.log(computerSelection);
-    // console.log(comBine);
     switch(comBine) {
         case 'ROCK SCISSORS':
         case 'PAPER ROCK':
         case 'SCISSORS PAPER':
-            result = `YOU WIN! ${finSelection} beats ${computerSelection}`;
+            result = 'YOU WIN!';
             break;    
         case 'ROCK ROCK':
         case 'PAPER PAPER':
         case 'SCISSORS SCISSORS':
-            result = `DRAW`;
+            result = 'DRAW. Play this  round again';
+            break;
+        case 'ROCK PAPER':
+        case 'PAPER SCISSORS':
+        case 'SCISSORS ROCK':
+            result = 'YOU LOSE!';    
             break;
         default:
-            result = `YOU LOSE! ${computerSelection} beats ${finSelection}`;
+            result = `${finSelection} IS INVALID, PICK AGAIN`;
 
     }
 
     return result;
 }
 
-function game()
+
+function game() {
+    const playerSelection = prompt('Enter your Selection, Human', null);
+    const finSelection = playerSelection.toUpperCase();
+    const computerSelection = getComputerChoice(arr);
+    console.log( playRound (finSelection, computerSelection) );
+    return playRound (finSelection, computerSelection);
+}
+
+
+let human = 0;
+let computer = 0;
+let invResult;
+
+
+function outcome() {
+let round = game();
+if(round === 'YOU WIN!') {
+    human++;
+} else if (round === 'YOU LOSE!') {
+    computer++;
+} else {
+    invResult = outcome();
+}
+}
 
 
 
+outcome();
+outcome();
+outcome();
+outcome();
+outcome();
 
-const playerSelection = 'PAPER';
-const computerSelection = getComputerChoice(arr);
-console.log(playRound (playerSelection, computerSelection) );
+console.log(`You won ${human} rounds.`);
+console.log(`Computer won ${computer} rounds.`);
+
+function smarterBeing() {
+    if (human > computer) {
+        alert('You win. You are smarter than your computer');
+    } else if (computer > human) {
+        alert('Your Computer wins. Your computer seems to outsmart you, Human');
+    } else {
+        alert('DRAW');
+    }
+}
+
+smarterBeing();
